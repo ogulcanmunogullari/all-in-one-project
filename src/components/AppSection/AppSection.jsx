1133aac64635465eabdfe4fbb249989b9d6f2e2d
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2022-09-06 03:17:41 +0400

    mobile changes:
    if there is no todos or notes looking bad, adding here to little card to information.

--- src/components/AppSection/AppSection.jsx
+++ src/components/AppSection/AppSection.jsx
@@ -3,11 +3,16 @@ import React, { useState, memo } from "react"
 import Todos from "./Todos/Todos"
 import Notes from "./Notes/Notes"
 import Dialog from "../Form/Timer/Dialog"
+import { useSelector } from "react-redux"
 //Icons
 import { LeftIcon, RightIcon } from "../../Icons/Icons"
 
 function AppSection() {
   const [mobileState, setMobileState] = useState(0)
+  const todo = useSelector((state) => state.todoSlicer.todos)
+  const note = useSelector((state) => state.noteSlicer.notes)
+  const theme = useSelector((state) => state.themeSlicer.theme)
+  console.log(theme)
 
   const leftHandle = () => {
     if (mobileState === 0) {
@@ -40,12 +45,33 @@ function AppSection() {
           <div className="w-full">
             {mobileState === -1 && (
               <div className="my-5">
-                <Notes />
+                {note.length > 0 ? (
+                  <Notes />
+                ) : (
+                  <div
+                    className={`flex justify-center items-center py-28 
+                   ${
+                     theme === true
+                       ? "bg-slate-200"
+                       : "bg-slate-500 text-slate-100"
+                   }`}>
+                    There is no notes.
+                  </div>
+                )}
               </div>
             )}
             {mobileState === 0 && (
               <div className="my-5">
-                <Todos />
+                {todo.length > 0 ? (
+                  <Todos />
+                ) : (
+                  <div
+                    className={`flex justify-center items-center  py-28 ${
+                      theme ? "bg-slate-200" : "bg-slate-500 text-slate-100"
+                    }`}>
+                    There is no todos.
+                  </div>
+                )}
               </div>
             )}
             {mobileState === 1 && (
