2fc4dd65e26f3c809412dd3a3ff0d137c73a16bd
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2023-06-21 22:15:23 +0300

    change title and favicon to dynamic results

--- src/components/Form/Timer/Dialog.jsx
+++ src/components/Form/Timer/Dialog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import { useState } from "react"
 import Button from "@mui/material/Button"
 import Sound from "../../../alert-sound/trumpet.mp3"
 import { useSelector } from "react-redux"
@@ -8,6 +8,7 @@ function Dialog() {
   const [interv, setInterv] = useState()
   const [status, setStatus] = useState(0)
   const theme = useSelector((state) => state.themeSlicer.theme)
+
   function playSound() {
     const audio = new Audio(Sound)
     audio.play()
@@ -18,6 +19,7 @@ function Dialog() {
     run()
     setStatus(1)
     setInterv(setInterval(run, 1000))
+    changeIcon("timer")
   }
   function stop() {
     clearInterval(interv)
@@ -27,47 +29,67 @@ function Dialog() {
     clearInterval(interv)
     setStatus(0)
     setTime({ m: 0, s: 0 })
+    document.title = "All in one App"
+    changeIcon("todo")
   }
   function resume() {
     start()
   }
   if (time.m <= 0 && time.s <= 0 && status === 1) {
     reset()
+    document.title = "Time's Up"
+    setTimeout(() => {
+      document.title = "All in one App"
+      changeIcon("todo")
+    }, 5000)
     playSound()
   }
-
+  function changeIcon(selection) {
+    let link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+    if (selection === "timer") {
+      link.href = "timer.png";
+    } else {
+      link.href = "todo.png";
+    }
+  }
+  function changeTitle(updatedMinute, updatedSecond) {
+    document.title = (updatedMinute < 10 ? "0" + updatedMinute : updatedMinute) + " : " + (updatedSecond < 10 ? "0" + updatedSecond : updatedSecond);
+  }
   function run() {
     if (updatedSecond === 0) {
       updatedMinute--
       updatedSecond = 59
+      changeTitle(updatedMinute, updatedSecond)
       return setTime({ m: updatedMinute, s: updatedSecond })
     }
     updatedSecond--
+    changeTitle(updatedMinute, updatedSecond)
     setTime({ m: updatedMinute, s: updatedSecond })
   }
 
   return (
     <div
-      className={`${
-        theme ? "bg-slate-200" : "bg-slate-500"
-      } p-5 w-full flex flex-col gap-5`}>
+      className={`${theme ? "bg-slate-200" : "bg-slate-500"
+        } p-5 w-full flex flex-col gap-5`}>
       <div className="flex flex-row justify-center">
         <span
-          className={`${
-            theme ? "text-slate-800" : "text-slate-300"
-          } text-7xl font-semibold`}>
+          className={`${theme ? "text-slate-800" : "text-slate-300"
+            } text-7xl font-semibold`}>
           {time.m < 10 ? "0" + time.m : time.m}
         </span>
         <span
-          className={`${
-            theme ? "text-slate-800" : "text-slate-300"
-          } text-7xl  font-semibold -mt-2`}>
+          className={`${theme ? "text-slate-800" : "text-slate-300"
+            } text-7xl  font-semibold -mt-2`}>
           :
         </span>
         <span
-          className={`${
-            theme ? "text-slate-800" : "text-slate-300"
-          } text-7xl font-semibold`}>
+          className={`${theme ? "text-slate-800" : "text-slate-300"
+            } text-7xl font-semibold`}>
           {time.s < 10 ? "0" + time.s : time.s}
         </span>
       </div>
